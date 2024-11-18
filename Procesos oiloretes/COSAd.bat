ca1096a4fe7f5924104cbe7285f67fd88f930dcc
Cootleam <beltran.fernandez.pisos@iescastelar.com>
2024-11-18 09:43:23 +0100

    Add files via upload

---
+++ Procesos oiloretes/COSAd.bat
@@ -0,0 +1,4 @@
+@echo off
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti %USERPROFILE%\ASMR
+mkdir %USERPROFILE%\asir\doct pract exam %USERPROFILE%\dam\docs par exdam %USERPROFILE%\DAM\doct HA hue %USERPROFILE%\ceti\doct prar enam %USERPROFILE%\ASMR\doct pra exen
