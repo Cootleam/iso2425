eca3038e1af862346638d6a7a296d1ddced562f9
Cootleam <beltran.fernandez.pisos@iescastelar.com>
2024-11-19 09:18:05 +0100

    Update COSAd.bat

--- Procesos oiloretes/COSAd.bat
+++ Procesos oiloretes/COSAd.bat
@@ -1,4 +1,6 @@
 @echo off
 
 mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti %USERPROFILE%\ASMR
-mkdir %USERPROFILE%\asir\doct pract exam %USERPROFILE%\dam\docs par exdam %USERPROFILE%\DAM\doct HA hue %USERPROFILE%\ceti\doct prar enam %USERPROFILE%\ASMR\doct pra exen
+mkdir %USERPROFILE%\asir\doct%USERPROFILE%\dam\docs %USERPROFILE%\DAM\doc %USERPROFILE%\ceti\doct %USERPROFILE%\ASMR\doct
+mkdir %USERPROFILE%\asir\pract %USERPROFILE%\dam\par %USERPROFILE%\DAM\HA %USERPROFILE%\ceti\prar %USERPROFILE%\ASMR\pra
+mkdir %USERPROFILE%\asir\exam %USERPROFILE%\dam\exdam %USERPROFILE%\DAM\hue %USERPROFILE%\ceti\enam %USERPROFILE%\ASMR\exen
